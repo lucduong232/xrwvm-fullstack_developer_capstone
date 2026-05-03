63088d4793fb900d03c3c78b885652964f241c64
lucduong232 <dl2322005@gmail.com>
2026-05-03 13:05:56 +0700

    Add files via upload

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,22 +5,46 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 const axios = require('axios'); // Quan trọng: Phải có dòng này
 
-// Task 10: Get the list of books available in the shop using async-await
+// Task 6: Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.status(200).json(books[isbn].reviews);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+// Task 7: Register a new user
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    const userExists = users.filter((user) => user.username === username);
+    if (userExists.length === 0) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
+});
+/ Task 10: Get the list of books available in the shop using async-await
 public_users.get('/', async function (req, res) {
   try {
-    // Trong thực tế lab, bạn có thể gọi đến chính endpoint của mình hoặc trả về promise
-    const getBooks = () => Promise.resolve(books);
-    const result = await getBooks();
-    res.status(200).json(result);
+    // Giả lập gọi API lấy danh sách sách bằng async/await
+    const response = await Promise.resolve(books);
+    res.status(200).json(response);
   } catch (error) {
-    res.status(500).json({message: "Error retrieving books"});
+    res.status(500).json({ message: "Error retrieving books" });
   }
 });
 
 // Task 11: Get book details based on ISBN using Promises
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  const getBook = new Promise((resolve, reject) => {
+  const getBookByISBN = new Promise((resolve, reject) => {
     if (books[isbn]) {
       resolve(books[isbn]);
     } else {
@@ -28,19 +52,23 @@ public_users.get('/isbn/:isbn', function (req, res) {
     }
   });
 
-  getBook
+  getBookByISBN
     .then((book) => res.status(200).json(book))
-    .catch((err) => res.status(404).json({message: err}));
+    .catch((err) => res.status(404).json({ message: err }));
 });
 
 // Task 12: Get book details based on author using async-await
 public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
   try {
-    const getBooksByAuthor = Object.values(books).filter(b => b.author === author);
-    res.status(200).json(getBooksByAuthor);
+    const getBooks = new Promise((resolve) => {
+      const filteredBooks = Object.values(books).filter(b => b.author === author);
+      resolve(filteredBooks);
+    });
+    const result = await getBooks;
+    res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({message: "Error retrieving books by author"});
+    res.status(500).json({ message: "Error retrieving books by author" });
   }
 });
 
@@ -58,7 +86,17 @@ public_users.get('/title/:title', function (req, res) {
 
   getByTitle
     .then(result => res.status(200).json(result))
-    .catch(err => res.status(404).json({message: err}));
+    .catch(err => res.status(404).json({ message: err }));
+});
+
+// Task 6: Get book review (Đảm bảo vẫn giữ code này)
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.status(200).json(books[isbn].reviews);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
